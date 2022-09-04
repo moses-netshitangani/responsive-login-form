cc4f03a2ac2b4c7e77c1bc1f29b61b32ac6be7f4
Moses Netshitangani <netshitanganimoses@gmail.com>
2022-09-04 14:01:47 +0200

    fix: responsive

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
 import "./loginform.css"
 
 const LoginForm = () => {
+
+    useEffect(() => {
+        function start() {
+            gapi.client.init({
+                clientId: "79474543031-tmjo35916ufn421ej3u1i2ljao2apr4s.apps.googleusercontent.com",
+                scope: ""
+            })
+        }
+        gapi.load('client: auth2', start)
+    })
 
     const [popupStyle, showPopup] = useState("hide")
 
     const popup = () => {
         showPopup("login-popup")
         setTimeout(() => showPopup("hide"), 3000)
+    }
+
+    const onSuccess = e => {
+        alert("User signed in")
+        console.log(e)
+    }
+
+    const onFailure = e => {
+        alert("User sign in Failed")
+        console.log(e)
     }
 
     return (
@@ -22,14 +44,25 @@ const LoginForm = () => {
 
             <div className="alt-login">
                 <div className="facebook"></div>
-                <div className="google"></div>
+                <div className="google">
+                    <GoogleLogin className="blue"
+                        clientId="79474543031-tmjo35916ufn421ej3u1i2ljao2apr4s.apps.googleusercontent.com"
+                        buttonText=""
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}
+                        cookiePolicy={'single_host_origin'}
+                        isSignedIn={false} // alternative is true, which keeps the user signed in
+                        icon={false}    // alt is true, and this puts the google logo on your button, but I don't like it
+                        theme="dark"  // alternative is light, which is white
+                    />
+                </div>
             </div>
 
             <div className={popupStyle}>
                 <h3>Login Failed</h3>
                 <p>Username or password incorrect</p>
             </div>
-
+            
         </div>
     )
 }
